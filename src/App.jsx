737e581d6eaafaed1e9ff90b502eb0ce231e1ec6
Ariel <arie.michalik@gmail.com>
2024-09-12 21:36:53 +0200

    add some dependencies

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
+import styled from "styled-components";
+
+const H1 = styled.h1`
+  font-size: 30px;
+  font-weight: 500;
+`;
 function App() {
-  return <div>simea</div>;
+  return <H1>hello react</H1>;
 }
 
 export default App;
