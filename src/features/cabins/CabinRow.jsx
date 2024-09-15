e46e6ce5bfbc65f08f1c9a7c140756a485f2ec43
Ariel <arie.michalik@gmail.com>
2024-09-15 09:19:05 +0200

    add toast notifications

--- src/features/cabins/CabinRow.jsx
+++ src/features/cabins/CabinRow.jsx
@@ -2,6 +2,7 @@ import styled from "styled-components";
 import { formatCurrency } from "../../utils/helpers";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { deleteCabin } from "../../services/apiCabins";
+import toast from "react-hot-toast";
 
 const TableRow = styled.div`
   display: grid;
@@ -58,13 +59,13 @@ function CabinRow({ cabin }) {
     //deleteCabin is an mutation func
     mutationFn: (id) => deleteCabin(id),
     onSuccess: () => {
-      alert("Cabin sucessfully deleted");
+      toast.success("Cabin sucessfully deleted");
       queryClient.invalidateQueries({
         queryKey: ["cabins"],
       });
     },
     //takes error from a mutation func
-    onError: (err) => alert(err.message),
+    onError: (err) => toast.error(err.message),
   });
   return (
     <TableRow role="row">
